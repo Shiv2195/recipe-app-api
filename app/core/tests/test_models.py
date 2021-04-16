4d51db4a70c68085c29d26ed24831e5571ce485e
Shiv <treadwill.iitk.2@gmail.com>
2021-04-16 12:18:17 +0530

    added working list tags endpoint

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,11 +1,17 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
+from core import models
+
+
+def sample_user(email='shibull@gmail.com', password='test123'):
+    return get_user_model().objects.create_user(email, password)
+
 
 class ModelTests(TestCase):
     def test_create_user_with_email_successful(self):
         email = 'shibull@gmail.com'
-        password = 'Test123'
+        password = 'test123'
 
         user = get_user_model().objects.create_user(
             email=email, password=password
@@ -15,13 +21,11 @@ class ModelTests(TestCase):
         self.assertTrue(user.check_password(password))
 
     def test_new_user_email_normalized(self):
-
         email = 'test@SHIBULL.com'
         user = get_user_model().objects.create_user(email, 'test123')
         self.assertEqual(user.email, email.lower())
 
     def test_new_user_invalid_email(self):
-
         with self.assertRaises(ValueError):
             get_user_model().objects.create_user(None, 'test123')
 
@@ -30,3 +34,10 @@ class ModelTests(TestCase):
 
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
+
+    def test_tag_str(self):
+        tag = models.Tag.objects.create(
+            user=sample_user(),
+            name='Vegan'
+        )
+        self.assertEqual(str(tag), tag.name)
